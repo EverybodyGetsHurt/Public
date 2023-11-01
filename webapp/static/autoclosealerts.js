05d068a1b779b46f6aeeb5effe18ad9774d04eea
Benemortasia <59669136+EverybodyGetsHurt@users.noreply.github.com>
2023-11-01 23:51:03 +0100

    Minified

--- webapp/static/autoclosealerts.js
+++ webapp/static/autoclosealerts.js
@@ -1,5 +1,1 @@
-$(document).ready(function(){
-    setTimeout(function(){
-        $('.alert').alert('close');
-    }, 2000); // The alerts will close after 2000 milliseconds, or 2 seconds
-});
+$(document).ready(function(){setTimeout(function(){$(".alert").alert("close")},2e3)});
