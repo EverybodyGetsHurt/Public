ac834147cdebb96fdbab5093999053b6f68bf63a
Benemortasia <59669136+EverybodyGetsHurt@users.noreply.github.com>
2023-11-12 23:53:23 +0100

    Update oauth20pkce.py

--- webapp/oauth20pkce.py
+++ webapp/oauth20pkce.py
@@ -15,7 +15,6 @@ import requests  # Module for making HTTP requests.
 from .models import db, OAuth20PKCE  # Importing database and model related classes.
 from sqlalchemy.sql import func  # SQLAlchemy's func module is used for SQL functions.
 
-
 # Initializing the Flask application instance with instance_relative_config set to True.
 # This allows the app to load configuration files relative to the instance folder.
 app = Flask(__name__, instance_relative_config=True)
@@ -95,46 +94,70 @@ def oauth20pkce_index():
 # and handles errors, validates the state parameter, and exchanges the authorization code for an access token.
 # It then uses this token to retrieve the user's information and handles the response accordingly,
 # either updating an existing user's data or creating a new user record.
-@oauth20pkce.route('/oauth20pkcecallback')
+@login_required  # Ensures that only authenticated users can access this route.
+@oauth20pkce.route('/oauth20pkcecallback')  # Maps the URL '/oauth20pkcecallback' to this function.
 def oauth20pkcecallback():
-    # The user's email is retrieved from the current_user object provided by Flask-Login.
+    """
+    The 'oauth20pkcecallback' function serves as the callback endpoint for the OAuth 2.0 PKCE flow. It is executed
+    after the user authorizes the application on the OAuth 2.0 authorization server. This function primarily
+    handles the exchange of an authorization code for an access token and uses this token to retrieve the user's
+    information from the OAuth server.
+
+    The function follows these steps:
+    1. Validates the current user's authentication status.
+    2. Handles any errors returned by the OAuth server.
+    3. Retrieves and validates the authorization code and state from the callback query parameters.
+    4. Exchanges the authorization code for an access token.
+    5. Retrieves the user's information using the access token.
+    6. Updates or creates a user record in the database with the retrieved information.
+    """
+
+    # This block checks if the current user is authenticated using Flask-Login's current_user proxy.
+    # If authenticated, the user's email is retrieved for later use. Otherwise, email is set to None.
     if current_user.is_authenticated:
         email = current_user.email
     else:
-        email = None  # or handle this case as per your application's requirement
+        email = None
 
-    # Error handling is in place to catch and log any errors that occur during the OAuth process,
-    # ensuring a graceful failure that can be diagnosed and corrected.
+    # Error handling for OAuth errors. If any error is returned in the query parameters from the
+    # OAuth server, it is logged, and the user is redirected to an error page.
     error = request.args.get('error')
     if error:
-        # Extracting the code and state from the query string
         app.logger.error(f'Authorization failed: {error}')
         flash('Authorization failed', 'danger')
         return redirect(url_for('unauth.unauthhome'))
 
+    # Retrieving the authorization code and state from the query parameters of the callback URL.
+    # These are essential for the PKCE flow, where the code is exchanged for an access token, and
+    # the state is used to mitigate CSRF attacks.
     code = request.args.get('code')
-    # The state parameter returned from the authorization server is validated against the one stored
-    # in the user's session to mitigate CSRF attacks.
     state = request.args.get('state')
     app.logger.info(f"Received code: {code}")
     app.logger.info(f"Received state: {state}")
 
-    # If the state parameter from the server matches the one stored in the session, the process continues
-    # Otherwise, an error is logged and the user is redirected
+    # Validating the state parameter against the value stored in the user's session.
+    # This is a crucial security measure to prevent CSRF attacks.
     if state != session.get('state'):
         app.logger.warning('Invalid state parameter')
         flash('Invalid state parameter', 'danger')
         return redirect(url_for('unauth.unauthabout'))
 
-    # Retrieving the stored code verifier from the session
+    # Retrieving the code verifier from the user's session. This verifier was previously generated
+    # and stored in the session during the initial phase of the OAuth 2.0 PKCE flow.
     code_verifier = session.get('code_verifier')
     app.logger.info(f"Code Verifier from session: {code_verifier}")
+
+    # Handling cases where the code verifier is not found or invalid.
+    # This is a critical part of the PKCE flow, as the code verifier ensures the security of the code exchange process.
     if not code_verifier:
         app.logger.warning('Invalid code verifier')
         flash('Invalid code verifier', 'danger')
         return redirect(url_for('unauth.unauthhome'))
 
-    # Creating a request to exchange the authorization code for an access token
+    # Setting up the request to the OAuth server's token endpoint to exchange the authorization code
+    # for an access token. This process is secured by including the previously stored code verifier.
+    # The request includes necessary headers and data, such as the client credentials, grant type, code,
+    # redirect URI, and the code verifier.
     token_url = 'https://api.twitter.com/2/oauth2/token'
     credentials = base64.urlsafe_b64encode(
         f"{app.config['CONSUMER_KEY']}:{app.config['CONSUMER_SECRET']}".encode()
@@ -151,30 +174,24 @@ def oauth20pkcecallback():
         'redirect_uri': app.config['REDIRECT_URI'],
         'code_verifier': code_verifier
     }
-
-    # The headers and data for the token request are logged for debugging purposes
     app.logger.info(f"Token request headers: {headers}")
     app.logger.info(f"Token request data: {data}")
 
-    # The application exchanges the authorization code for an access token using a POST request,
-    # including the code_verifier to satisfy the PKCE requirement.
+    # Sending the POST request to the token endpoint. The response is checked for a successful status code.
+    # If the response indicates a failure, an error is logged, and the user is redirected.
     response = requests.post(token_url, headers=headers, data=data)
-
-    # If the request is unsuccessful, an error is logged and the user is redirected
     if response.status_code != 200:
         app.logger.error(f'Failed to exchange authorization code for access token: {response.text}')
         flash(f"Authorization failed: {response.text}", 'danger')
         return redirect(url_for('unauth.unauthhome'))
 
-    # The access token and user information are logged for debugging purposes
+    # Parsing the response to extract the access token. This token is then used to fetch the user's information
+    # from the OAuth server. The headers for this request include the Bearer token authorization.
     token_data = response.json()
-
-    # Using the access token to fetch the user's information
     headers = {
         'Authorization': f"Bearer {token_data['access_token']}"
     }
     user_info_url = 'https://api.twitter.com/2/users/me'
-    # The user's information is retrieved from the authorization server using the newly acquired access token.
     user_response = requests.get(user_info_url, headers=headers)
 
     app.logger.info(f"Access Token: {token_data['access_token']}")
@@ -182,57 +199,53 @@ def oauth20pkcecallback():
     app.logger.info(f"User info response headers: {user_response.headers}")
     app.logger.info(f"User info response text: {user_response.text}")
 
-    # If the request for user information is unsuccessful, an error is logged and the user is redirected
+    # Handling unsuccessful requests for user information.
     if user_response.status_code != 200:
         app.logger.error(f'Failed to retrieve user information: {user_response.text}')
         flash('Failed to retrieve user information', 'danger')
         return redirect(url_for('unauth.unauthhome'))
 
-    # Parsing the JSON response to get the user's data
+    # Parsing the user information response. This data is used to either update an existing user's record
+    # or create a new user in the application's database.
     user_data = user_response.json()
 
-    # Existing users are updated with new tokens and other info, while new users are created as needed,
-    # ensuring a smooth and seamless user experience.
+    # Searching for an existing user in the database by email.
     existing_user = OAuth20PKCE.query.filter_by(email=email).first()
 
-    # If a user with the given email already exists, their information is updated
-    # Otherwise, a new user record is created
+    # If an existing user is found, their record is updated. Otherwise, a new user record is created.
     if existing_user:
-        # Update the existing user record with new tokens and other info
+        # Update logic for existing user record.
         existing_user.account_name = user_data['data']['name']
         existing_user.access_token = token_data['access_token']
-        existing_user.refresh_token = token_data.get('refresh_token', existing_user.refresh_token)  # Update if present
+        existing_user.refresh_token = token_data.get('refresh_token', existing_user.refresh_token)
         existing_user.state = state
         existing_user.code_verifier = code_verifier
         existing_user.twitter_id = user_data['data']['id']
-        existing_user.last_token_refresh_date = func.now()  # Update the last token refresh date
+        existing_user.last_token_refresh_date = func.now()
         db.session.commit()
         flash('Authorization successful', 'success')
         return render_template("oauth20pkcecallback.html", user_data=user_data, user=current_user, email=email)
     else:
-        # Create and store the OAuth record after processing the callback
+        # Logic for creating a new user record.
         oauth_record = OAuth20PKCE(
             email=email,
             account_name=user_data['data']['name'],
             access_token=token_data['access_token'],
-            refresh_token=token_data.get('refresh_token'),  # Store refresh token if present
+            refresh_token=token_data.get('refresh_token'),
             state=state,
             code_verifier=code_verifier,
             twitter_id=user_data['data']['id'],
-            # Add any other necessary fields
+            # Additional fields can be added as needed.
         )
 
+        # Attempting to add the new user record to the database.
         try:
-            # The new OAuth record is added to the database, and the transaction is committed
             db.session.add(oauth_record)
             db.session.commit()
-
-            # A success message is flashed to the user, and they are presented with a webpage displaying their user data
             flash('Authorization successful', 'success')
             return render_template("oauth20pkcecallback.html", user_data=user_data, user=current_user, email=email)
         except IntegrityError as e:
-            # If a database error occurs, the transaction is rolled back
-            # The user is flashed with an error message and redirected
+            # Handling database errors, particularly related to unique constraint violations.
             db.session.rollback()
             if "UNIQUE constraint failed: OAuth20_PKCE.twitter_id" in str(e):
                 flash('This Twitter ID is already registered with another account.', 'error')
